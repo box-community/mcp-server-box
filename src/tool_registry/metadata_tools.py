0c42b318c269473c678375eb9265984e93896ad5
Rui Barbosa <barduinor@users.noreply.github.com>
2025-11-13 16:37:02 -0500

    RB/20251113 folders (#30)
    * Updating box ai toolkit version to 0.1.3
    
    * Implementing Folder tools
    
    * Ruff
    
    * Update documentation
    
    * Update documentation

--- src/tool_registry/metadata_tools.py
+++ src/tool_registry/metadata_tools.py
@@ -1,14 +1,14 @@
 from mcp.server.fastmcp import FastMCP
 
 from tools.box_tools_metadata import (
+    box_metadata_delete_instance_on_file_tool,
+    box_metadata_get_instance_on_file_tool,
+    box_metadata_set_instance_on_file_tool,
     box_metadata_template_create_tool,
-    box_metadata_template_list_tool,
     box_metadata_template_get_by_key_tool,
     box_metadata_template_get_by_name_tool,
-    box_metadata_set_instance_on_file_tool,
-    box_metadata_get_instance_on_file_tool,
+    box_metadata_template_list_tool,
     box_metadata_update_instance_on_file_tool,
-    box_metadata_delete_instance_on_file_tool,
 )
 
 
